49e88c168e732414fe47a0957166d42085d73b40
Azharruddin Khan <aakhan@mrccsolutions.com>
2017-07-07 13:01:44 +0530

    commit

--- js/javasript.js
+++ js/javasript.js
@@ -1,8 +1,68 @@
 $(function () {
+  var localip;
+
+  function getIPs(callback) {
+    var ip_dups = {};
+
+    var RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+    var useWebKit = !!window.webkitRTCPeerConnection;
+
+    if (!RTCPeerConnection) {
+
+      var win = iframe.contentWindow;
+      RTCPeerConnection = win.RTCPeerConnection || win.mozRTCPeerConnection || win.webkitRTCPeerConnection;
+      useWebKit = !!win.webkitRTCPeerConnection;
+    }
+
+    var mediaConstraints = {
+      optional: [{ RtpDataChannels: true }]
+    };
+
+    var servers = { iceServers: [{ urls: "stun:stun.services.mozilla.com" }] };
+
+    var pc = new RTCPeerConnection(servers, mediaConstraints);
+
+    function handleCandidate(candidate) {
+      var ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
+      var ip_addr = ip_regex.exec(candidate)[1];
+
+      if (ip_dups[ip_addr] === undefined)
+        callback(ip_addr);
+
+      ip_dups[ip_addr] = true;
+    }
+
+    pc.onicecandidate = function (ice) {
+
+      if (ice.candidate)
+        handleCandidate(ice.candidate.candidate);
+    };
+
+    pc.createDataChannel("");
+
+    pc.createOffer(function (result) {
+
+      pc.setLocalDescription(result, function () {}, function () {});
+
+    }, function () {});
+
+    setTimeout(function () {
+      var lines = pc.localDescription.sdp.split('\n');
+
+      lines.forEach(function (line) {
+        if (line.indexOf('a=candidate:') === 0)
+          handleCandidate(line);
+      });
+    }, 1000);
+  }
+  getIPs(function (ip) {
+    localip = ip;
+  });
   $("#send").click(function () {
     var msgbox = $("#textmsg").val();
     ref.push({
-      comment: msgbox.split("\n").join("<br/>")
+      comment: msgbox.split("\n").join("<br/>"),
+      ip: localip
     });
     $("#textmsg").val("");
   });
@@ -10,29 +70,16 @@ $(function () {
     if (data.val().url) {
       $(".chatDiv ul").append("<li><img class='myImg' src='" + data.val().url + "'/></li>");
       var modal = $("#myModal");
-
-      // Get the image and insert it inside the modal - use its "alt" text as a caption
       var img = $(".myImg");
       var modalImg = $("#img01");
-      // img.onclick = function () {
-      //   modal.style.display = "block";
-      //   modalImg.src = this.src;
-      // }
-      img.click(function(){
+      img.click(function () {
         var store = $(this).attr("src");
-        modal.css("display","block");
-        console.log(store);
-        modalImg.attr("src",store);
+        modal.css("display", "block");
+        modalImg.attr("src", store);
       });
-      // Get the <span> element that closes the modal
       var span = document.getElementsByClassName("close")[0];
-
-      // When the user clicks on <span> (x), close the modal
-      // span.onclick = function () {
-      //   modal.style.display = "none";
-      // }
-      span.click(function(){
-        modal.css("display","none");
+      span.click(function () {
+        modal.css("display", "none");
       });
     } else {
       $(".chatDiv ul").append("<li><b>anonymous:</b> " + data.val().comment + "</li>");
